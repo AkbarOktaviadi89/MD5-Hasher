b5ebd79eb287f6546eba7943092a27f9d750fe46
AkbarOktaviadi89 <Akbaroktaviadi89@gmail.com>
2025-07-02 23:22:07 +0700

    feat: add compare md5 function

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import hashlib
 
 def generate_md5(text):
     return hashlib.md5(text.encode()).hexdigest()
+
+def compare_md5(text, hash_to_compare):
+    return generate_md5(text) == hash_to_compare.lower()
+
+
