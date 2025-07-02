c07384570e86d51d16357583b75169f8863660e3
AkbarOktaviadi89 <Akbaroktaviadi89@gmail.com>
2025-07-02 23:23:22 +0700

    feat: add choice function in main

--- main.py
+++ main.py
@@ -17,5 +17,15 @@ def main():
         hash_result = generate_md5(text)
         print(f"MD5 Hash: {hash_result}")
 
+    elif choice == '2':
+        text = input("Masukkan teks asli: ")
+        md5_hash = input("Masukkan hash MD5: ")
+        if compare_md5(text, md5_hash):
+            print("✅ Teks cocok dengan hash MD5!")
+        else:
+            print("❌ Teks TIDAK cocok dengan hash MD5.")
+    else:
+        print("Pilihan tidak valid.")
+
 if __name__ == "__main__":
     main()
